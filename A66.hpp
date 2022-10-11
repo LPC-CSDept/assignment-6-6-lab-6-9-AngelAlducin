e9a126e4921f5411fbc705c936b0d2806b2d6f83
AngelAlducin <aualducin@gamil.com>
2022-10-11 16:20:50 -0700

    Done

--- A66.hpp
+++ A66.hpp
@@ -5,3 +5,36 @@
 using namespace std;
 
 // complete the function maxmin( )
+
+int maxmin(int n1, int n2, int n3, int &min, int &max)
+{
+if((n1 >= n3) && (n1 >= n2))
+{
+    max = n1;
+}
+
+else if((n2 >= n3) && (n2 >= n1))
+{
+max = n2;
+}
+
+else 
+{
+max = n3;
+}
+
+if((n1 <= n3) && (n1 <= n2))
+{
+min = n1;
+}
+
+else if((n2 <= n3) && (n2 <= n1))
+{
+min = n2;
+}
+
+else
+{
+min = n3;
+}
+}
